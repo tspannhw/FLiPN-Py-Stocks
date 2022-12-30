e2817dd6e7d7f91002a52224ec03e97a630f2d14
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2022-12-30 16:00:07 -0500

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -44,30 +44,37 @@ producer = client.create_producer(topic='persistent://public/default/stocks' ,sc
 
 
 def on_message(ws, message):
-    # print("MSG:" + message)
     stocks_dict = json.loads(message)
 
-    for stockitem in stocks_dict['data']:
-        print(stockitem['p'])
-        uuid_key = '{0}_{1}'.format(strftime("%Y%m%d%H%M%S",gmtime()),uuid.uuid4())
-        stockRecord = Stock()
-        stockRecord.symbol = stockitem['s']
-        stockRecord.ts = float(stockitem['t'])
-        stockRecord.currentts = float(strftime("%Y%m%d%H%M%S",gmtime()))
-        stockRecord.volume = float(stockitem['v'])
-        stockRecord.price = float(stockitem['p'])
-        stockRecord.tradeconditions = ' '.join(stockitem['c'])
-        stockRecord.uuid = uuid_key
-        producer.send(stockRecord,partition_key=str(uuid_key))
-        print(stockRecord)
-
+    try:
+        if stocks_dict is not None and "data" in stocks_dict:
+            for stockitem in stocks_dict['data']:
+                try:
+                    if stockitem is not None and stockitem['s'] is not None: 
+                        print(stockitem['p'])
+                        uuid_key = '{0}_{1}'.format(strftime("%Y%m%d%H%M%S",gmtime()),uuid.uuid4())
+                        stockRecord = Stock()
+                        stockRecord.symbol = stockitem['s']
+                        stockRecord.ts = float(stockitem['t'])
+                        stockRecord.currentts = float(strftime("%Y%m%d%H%M%S",gmtime()))
+                        stockRecord.volume = float(stockitem['v'])
+                        stockRecord.price = float(stockitem['p'])
+                        stockRecord.tradeconditions = ','.join(stockitem['c'])
+                        stockRecord.uuid = uuid_key
+                        if ( stockitem['s'] != '' ):
+                            producer.send(stockRecord,partition_key=str(uuid_key))
+                        print(stockRecord)
+                except NameError:
+                    print ("skip it")
+    except Exception as ex:
+        print (ex)
 
 def on_error(ws, error):
     print(error)
 
 
 def on_close(ws, close_status_code, close_msg):
-    print("### closed ###")
+    print("### closed websocket to finnhub ###")
     print(close_status_code)
     print(close_msg)
 
@@ -85,6 +92,59 @@ def on_open(ws):
     ws.send('{"type":"subscribe","symbol":"BABA"}')
     ws.send('{"type":"subscribe","symbol":"PYPL"}')
     ws.send('{"type":"subscribe","symbol":"EA"}')
+    ws.send('{"type":"subscribe","symbol":"WMT"}')
+    ws.send('{"type":"subscribe","symbol":"NKE"}')
+    ws.send('{"type":"subscribe","symbol":"BRK.B"}')
+    ws.send('{"type":"subscribe","symbol":"GOOGL"}')
+    ws.send('{"type":"subscribe","symbol":"UNH"}')
+    ws.send('{"type":"subscribe","symbol":"JNJ"}')
+    ws.send('{"type":"subscribe","symbol":"XOM"}')
+    ws.send('{"type":"subscribe","symbol":"JPM"}')
+    ws.send('{"type":"subscribe","symbol":"V"}')
+    ws.send('{"type":"subscribe","symbol":"HD"}')
+    ws.send('{"type":"subscribe","symbol":"LLY"}')
+    ws.send('{"type":"subscribe","symbol":"CVX"}')
+    ws.send('{"type":"subscribe","symbol":"ABBV"}')
+    ws.send('{"type":"subscribe","symbol":"PEP"}')
+    ws.send('{"type":"subscribe","symbol":"BAC"}')
+    ws.send('{"type":"subscribe","symbol":"KO"}')
+    ws.send('{"type":"subscribe","symbol":"MA"}')
+    ws.send('{"type":"subscribe","symbol":"AVGO"}')
+    ws.send('{"type":"subscribe","symbol":"TMO"}')
+    ws.send('{"type":"subscribe","symbol":"COST"}')
+    ws.send('{"type":"subscribe","symbol":"CSCO"}')
+    ws.send('{"type":"subscribe","symbol":"MCD"}')
+    ws.send('{"type":"subscribe","symbol":"ABT"}')
+    ws.send('{"type":"subscribe","symbol":"VZ"}')
+    ws.send('{"type":"subscribe","symbol":"DIS"}')
+    ws.send('{"type":"subscribe","symbol":"BMY"}')
+    ws.send('{"type":"subscribe","symbol":"CMCSA"}')
+    ws.send('{"type":"subscribe","symbol":"RTX"}')
+    ws.send('{"type":"subscribe","symbol":"HON"}')
+    ws.send('{"type":"subscribe","symbol":"IBM"}')
+    ws.send('{"type":"subscribe","symbol":"CVS"}')
+    ws.send('{"type":"subscribe","symbol":"ORCL"}')
+    ws.send('{"type":"subscribe","symbol":"CAT"}')
+    ws.send('{"type":"subscribe","symbol":"LOW"}')
+    ws.send('{"type":"subscribe","symbol":"BLK"}')
+    ws.send('{"type":"subscribe","symbol":"MS"}')
+    ws.send('{"type":"subscribe","symbol":"BA"}')
+    ws.send('{"type":"subscribe","symbol":"INTC"}')
+    ws.send('{"type":"subscribe","symbol":"INTU"}')
+    ws.send('{"type":"subscribe","symbol":"CB"}')
+    ws.send('{"type":"subscribe","symbol":"TMUS"}')
+    ws.send('{"type":"subscribe","symbol":"C"}')
+    ws.send('{"type":"subscribe","symbol":"DUK"}')
+    ws.send('{"type":"subscribe","symbol":"BDX"}')
+    ws.send('{"type":"subscribe","symbol":"SLB"}')
+    ws.send('{"type":"subscribe","symbol":"MMM"}')
+    ws.send('{"type":"subscribe","symbol":"CL"}')
+    ws.send('{"type":"subscribe","symbol":"TGT"}')
+    ws.send('{"type":"subscribe","symbol":"MRNA"}')
+    ws.send('{"type":"subscribe","symbol":"ICE"}')
+    ws.send('{"type":"subscribe","symbol":"USB"}')
+
+    print("openned websocket connection to finnhub")
     # ws.send('{"type":"subscribe","symbol":"BINANCE:BTCUSDT"}')
     # ws.send('{"type":"subscribe","symbol":"IC MARKETS:1"}')
 
@@ -93,7 +153,7 @@ if __name__ == "__main__":
     #websocket.enableTrace(True)
 
     websocket.enableTrace(False)
-    ws = websocket.WebSocketApp("wss://ws.finnhub.io?token=TokenFromFinnHub",
+    ws = websocket.WebSocketApp("wss://ws.finnhub.io?token=token",
                               on_message = on_message,
                               on_error = on_error,
                               on_close = on_close)
